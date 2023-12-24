68cfc510c3e0640b51d77e369aeca6d724e07cc2
kaisei0622 <ketchup.kaisei.0622@icloud.com>
2023-12-24 15:12:03 +0900

    課題修正

--- kadai_013/calculation.py
+++ kadai_013/calculation.py
@@ -1,8 +1,7 @@
 # 消費税を加えて計算
 def add_two_arguments(price, tax):
-
-  total = price * tax
-
-  print(f"{total}円")
+    total = price * tax
+    print(f"{total}円")
+    return total
 
 add_two_arguments(100, 1.1)
